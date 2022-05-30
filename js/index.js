c700a5c46ba5888739a434672f57b321832d488d
Gaston Marino <gmarino86@gmail.com>
2022-05-30 00:04:13 -0300

    Correcciones

--- js/index.js
+++ js/index.js
@@ -1,86 +1,88 @@
 const API_KEY = "ac313d72ccbe79ebc1acf7fff480f5be";
 const URL_WEATHER = "https://api.openweathermap.org/geo/1.0/direct?";
-const URL_WEATHER_CITY = "https://api.openweathermap.org/data/2.5/weather?"
+const URL_WEATHER_CITY = "https://api.openweathermap.org/data/2.5/weather?";
 
-const BTN_BUSCAR = document.getElementById('btnBuscar')
-const INPUT_TEXT = document.getElementById('inputText')
-
-
+const BTN_BUSCAR = document.getElementById("btnBuscar");
+const INPUT_TEXT = document.getElementById("inputText");
 
 BTN_BUSCAR.addEventListener("click", () => {
-    let ciudad = INPUT_TEXT.value
-
+  let ciudad = INPUT_TEXT.value;
+  if (ciudad) {
     fetchCiudad(ciudad)
-        .then(function (ciudades) {
-            //console.log(JSON.stringify(ciudades)
-            localStorage.setItem('city', JSON.stringify(ciudades))
-            armarInfo(ciudades)
-        })
-        .catch(function (err) {
-            console.log("Terminé con error: ", err);
-        });
+      .then(function (ciudades) {
+        localStorage.setItem("city", JSON.stringify(ciudades));
+        armarInfo(ciudades);
+      })
+      .catch(function (err) {
+        console.log("Terminé con error: ", err);
+      });
+  } else {
+    let main = document.getElementById("main");
+    main.innerText = "Ingrese un valor";
+  }
 });
 
-function fetchCiudad(ciudad)
-{
-    return fetch(`${URL_WEATHER}q=${ciudad}&limit=6&appid=${API_KEY}`)
-        .then(function (data) {
-            return data.json()
-        })
+function fetchCiudad(ciudad) {
+  return fetch(`${URL_WEATHER}q=${ciudad}&limit=6&appid=${API_KEY}`).then(
+    function (data) {
+      return data.json();
+    }
+  );
 }
 
 function armarInfo(ciudades) {
-    let main = document.getElementById('main')
-    let view = ''
-    view += '<div class="row">'
-    
-    for (let i = 0; i < ciudades.length; i++) {
-        view += `
+  console.log(ciudades);
+  let main = document.getElementById("main");
+  let view = "";
+  view += '<div class="row">';
+
+  for (let i = 0; i < ciudades.length; i++) {
+    view += `
         <div class="col-sm-4 mb-3">
-            <div class="card" onClick=listarCard(`
-        view += i
-        view += `)>
-                <div class="card-body">
-                    <h3 class="card-title">${ciudades[i].name}</h3>
-                    <p class="card-text">${ciudades[i].state}</p>
+            <div class="card" onClick=listarCard(`;
+    view += i;
+    view += `,${ciudades[i].lat},${ciudades[i].lon}`;
+    view += `)>
+        <div class="card-body">
+            <h3 class="card-title">${ciudades[i].name}</h3>
+            `;
+    if (ciudades[i].state) {
+      view += `<p class="card-text">${ciudades[i].state}</p>`;
+    }
+    view += `
                     <p class="card-text">${ciudades[i].country}</p>
                 </div>
             </div>
         </div>
-        `
-    }
-    view += '</div>'
+        `;
+  }
+  view += "</div>";
 
-    main.innerHTML = view
+  main.innerHTML = view;
 }
 
-function listarCard(i) {
-    
-    let ciudad = INPUT_TEXT.value
-
-    fetchCiudad(ciudad)
-    .then(function (ciudades) {
-        return fetch(`${ URL_WEATHER_CITY }lat=${ciudades[i].lat}&lon=${ciudades[i].lon}&appid=${API_KEY}&units=metric&lang=es`)
-    })
-    .then(function (data){
-        return data.json()
+function listarCard(lat, lon) {
+  return fetch(
+    `${URL_WEATHER_CITY}lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=es`
+  )
+    .then(function (data) {
+      return data.json();
     })
     .then(function (c) {
-        crearModal(c)
+      crearModal(c);
     })
     .catch(function (err) {
-        console.log("Terminé con error: ", err);
+      console.log("Terminé con error: ", err);
     });
 }
 
-function crearModal(c) 
-{
-    let data = c
-    console.log(c)
-    let divModal = document.getElementById('insertModal')
-    
-    let modal = ''
-    modal += `
+function crearModal(c) {
+  let data = c;
+  console.log(c);
+  let divModal = document.getElementById("insertModal");
+
+  let modal = "";
+  modal += `
     <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -88,20 +90,53 @@ function crearModal(c)
                     <h4 class="modal-title" id="staticBackdropLabel">${c.name}</h4>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <p><b>Temperatura:</b> ${c.main.temp} °C</p>
+                <div class="modal-body fondo`
+                
+                modal +=`">
+                <video autoplay="autoplay" loop="loop" id="video_background" preload="auto" volume="50"/>
+                `    
+                    switch (c.weather[0].main) {
+                        case 'Clouds':
+                            modal += `<source src="img/clouds.mp4" type="video/mp4" />`
+                            break;
+
+                        case 'Thunderstorm':
+                            modal += `<source src="img/rain.mp4" type="video/mp4" />`
+                            break;
+                        case 'Drizzle':
+                            modal += `<source src="img/rain.mp4" type="video/mp4" />`
+                            break;
+                        case 'Rain':
+                            modal += `<source src="img/rain.mp4" type="video/mp4" />`
+                            break;
+                        
+                        case 'Snow':
+                            modal += `<source src="img/snow.mp4" type="video/mp4" />`
+                            break;
+
+                        case 'Clear':
+                            modal += `<source src="img/sun.mp4" type="video/mp4" />`
+                            break;
+                        
+                        default:
+                            modal += `<source src="img/mist.mp4" type="video/mp4" />`
+                            break;
+                    }
+
+                modal +=`</video/>
+                    <p><b>Temperatura:</b> ${Math.round(c.main.temp)} °C</p>
                     <p>Hay ${c.weather[0].description}</p> 
                     
                     <div class="text-center">
                         <img src="http://openweathermap.org/img/wn/${c.weather[0].icon}@2x.png" alt="${c.weather[0].description}">
                     </div>
 
-                    <p><b>Temperatura Máxima:</b> ${c.main.temp_max}</p>
-                    <p><b>Temperatura Minima:</b> ${c.main.temp_min}</p>
+                    <p><b>Temperatura Máxima:</b> ${Math.round(c.main.temp_max)}</p>
+                    <p><b>Temperatura Minima:</b> ${Math.round(c.main.temp_min)}</p>
                     <p><b>Humedad:</b> ${c.main.humidity}</p>
-                    <p><b>Sensacion Térmica:</b> ${c.main.feels_like}</p>
+                    <p><b>Sensacion Térmica:</b> ${Math.round(c.main.feels_like)}</p>
                     <p><b>Presión atmosférica:</b> ${c.main.pressure}</p>
-                    <p><b>Velocidad del viento:</b> ${c.wind.speed} km/h</p>
+                    <p><b>Velocidad del viento:</b> ${Math.round(c.wind.speed)} km/h</p>
                     
                     </div>
                 <div class="modal-footer">
@@ -110,25 +145,29 @@ function crearModal(c)
             </div>
         </div>
     </div>
-    `
+    `;
 
-    divModal.innerHTML = modal
+  divModal.innerHTML = modal;
 
-    var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {
-        keyboard: false
-    })
+  var myModal = new bootstrap.Modal(document.getElementById("staticBackdrop"), {
+    keyboard: false,
+  });
 
-    myModal.show()
+  myModal.show();
 }
 
+init();
 
-
-function verLocalStorage()
-{
-    let ciudades = localStorage.getItem('city') 
-    if(ciudades != ''){
-        armarInfo(JSON.parse(ciudades))
-    }
+function init() {
+  verLocalStorage();
 }
 
-verLocalStorage()
+function verLocalStorage() {
+  let ciudades = localStorage.getItem("city");
+  if (ciudades != null) {
+    armarInfo(JSON.parse(ciudades));
+  } else {
+    let main = document.getElementById("main");
+    main.innerText = "Busque la temperatura de la ciudad que desee";
+  }
+}
